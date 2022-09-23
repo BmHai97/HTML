3e09136f0993c22f9093b2d5241a09b35375a1b6
BmHai97 <buiminhhai97@gmail.com>
2022-09-23 17:18:47 +0700

    upload 1

---
+++ js/vd1.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#hinh - ma").hover(function (param) {
+        $(this).attr("src", "imagines/index_Capture.png");
+    }, function () {
+    }, function () {
+        $t(this).attr("src", "imagines/index_hinh-anh-halloween-dep-de-thuong-kinh-di-5.jpg")
+    }
+
+     })
